89cda7623d761f751a3cc3a43df59803d4f3f7bd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-10-29 15:30:00 -0400

    Add project files.

---
+++ F24W8IntroToMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace F24W8IntroToMaui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
